e834b33210e3dd3ca26adbf957c0da396a41bb38
robertapeixoto <roberta.bittencourtpeixoto@utoledo.edu>
2026-03-12 15:42:35 -0400

    Saving LE CB inventory and L1 dataser
    updating the inventory csvs for LE and CB sites, saving the L2 file, and start updating the pipeline for the new dTmax code. I will keep working on that next time

--- create_inventory.R
+++ create_inventory.R
@@ -1,4 +1,6 @@
-#Script matching tree dbh with sapflow data
+#Script matching tree dbh with sapflow data in both CB and LE synoptic sites
+#The inventory CSVs were downloaded from the COMPASS drive - you must make sure they have the same column labels here for matching 
+#Roberta Peixoto in 3-12-2026 
 
 #Load packages
 library(tidyr)
@@ -6,23 +8,39 @@ library(readr)
 library(dplyr)
 library(stringr)
 
+
+#########################################   LE
 #Load dataframes
-sapflow_inventory <- read.csv("sapflow_inventory.csv")
-dbh_inventory <- read.csv("inventory.csv")
+LE_sapflow_inventory <- read.csv("LE_sapflow_inventory.csv")
+LE_dbh_inventory <- read.csv("LE_TreePlotInventory.csv")
 
-#Edit format to match dataframes
-#Remove trailing white spaces 
-sapflow_inventory %>%
-  mutate(Plot = ifelse(Plot == "SW", "Saltwater", Plot)) %>%
-  mutate(Plot = ifelse(Plot == "FW", "Freshwater", Plot)) %>%
-  mutate(Plot = trimws(Plot, which = "right")) -> sapflow_inventory
+#Merge dataframes 
+LE_inventory <- merge(LE_sapflow_inventory, LE_dbh_inventory, 
+                   by = c("Site", "Tag"), all.x = TRUE)%>%
+               dplyr::select(-"Table") # removing because there is no datalogger Table in the CB inventory
 
-#Merge dataframes and remove deep sapflow sensors
-inventory <- merge(sapflow_inventory, dbh_inventory, 
-                   by.x = c("Tree_ID", "Plot"), by.y = c("Tag", "Plot"), all.x = TRUE)
-inventory %>%
-  filter(!grepl("D", Sapflux_ID)) -> inventory
+#########################################   CB
+#Load dataframes
+CB_sapflow_inventory <- read.csv("CB_sapflow_inventory.csv")
+CB_dbh_inventory <- read.csv("CB_TreePlotInventory.csv")
 
+#Merge dataframes 
+CB_inventory <- merge(CB_sapflow_inventory, CB_dbh_inventory, 
+                      by = c("Site", "Tag"), all.x = TRUE)
+
+#########################################   Combinign info from LE and CB
+LE_inventory <- LE_inventory[, !grepl("^X", names(LE_inventory))]
+CB_inventory <- CB_inventory[, !grepl("^X", names(CB_inventory))]
+
+inventory_all <- rbind(LE_inventory, CB_inventory)%>%
+                 mutate(Sensor_ID = ifelse(str_detect(Tree_Code, "[-_]"),
+                                           str_extract(Tree_Code, "(?<=[-_])[^-_]+$"),
+                                           Tree_Code),
+                        Sensor_ID = str_replace(Sensor_ID, "^([0-9]+)D$", "\\1"),
+                        Sensor_ID = ifelse(str_detect(Sensor_ID, "^[0-9]+$"),
+                                           as.character(as.numeric(Sensor_ID)),
+                                           Sensor_ID),
+                        Sensor_ID = ifelse(Sensor_ID %in% c("", "D"), NA, Sensor_ID))
+  
 #Save as RDS
-saveRDS(inventory, file = "dbh.rds")
-      
+saveRDS(inventory_all, file = "dbh.rds")
